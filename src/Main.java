8b633c4fe561accb95a0b1ce8be3f1ef5539e743
21989 <2198965582@qq.com>
2025-05-15 19:19:34 +0800

    封装

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+class Student {
+    private String name;
+    private int age;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age) {
+        if (age <= 0) {
+            System.out.println("error");
+        }
+        else {
+            this.age = age;
+        }
+    }
+    void read(){
+        System.out.print("我是 "+name+",今年 "+age);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("yui");
+        s1.setAge(0);
+        s1.read();
+    }
+}
